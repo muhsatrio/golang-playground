13dccc70246ef42ac72fb363d1390a44eed02098
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2022-01-06 16:51:06 +0700

    update problem swapPairs

---
+++ leetcode/linkedlist/swapPairs.go
@@ -0,0 +1,40 @@
+package linkedlist
+
+func SwapPairs(head *ListNode) *ListNode {
+
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	var previousNode, tempNode, tempPreviousNode, tempNextNode, result *ListNode
+
+	node := head
+
+	swap := false
+
+	for node != nil {
+		if swap {
+			tempNode = node
+			tempPreviousNode = previousNode
+			tempNextNode = node.Next
+			tempNode.Next = nil
+			tempPreviousNode.Next = nil
+			result = Insert(result, tempNode.Val)
+			result = Insert(result, tempPreviousNode.Val)
+			tempNode.Next = tempPreviousNode
+			tempPreviousNode.Next = tempNextNode
+			node = tempNextNode
+			previousNode = tempPreviousNode
+			swap = false
+		} else {
+			if node.Next == nil {
+				Insert(result, node.Val)
+			}
+			previousNode = node
+			node = node.Next
+			swap = true
+		}
+	}
+
+	return result
+}
